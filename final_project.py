8702b936e046462eccbde6643576097587f497c2
smalden <104938127+smalden@users.noreply.github.com>
2022-05-05 00:08:24 -0400

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import pydeck as pdk
+
 us_states = {
         'AK': 'Alaska',
         'AL': 'Alabama',
@@ -70,9 +71,111 @@ mcDonalds = {
 }
 @st.cache(allow_output_mutation=True)
 def load_data(file_name):
-    data = pd.read_csv('fastfooddata.csv')
+    data = pd.read_csv('C:\\fastfooddata.csv')
     data.replace({'name': mcDonalds}, inplace=True)
     return data
+
+def map_choice(data):
+    city_choices = data.groupby('city')['name'].count().sort_values(ascending=False).head(15)
+    city_view = st.sidebar.selectbox("Select a city to view: ", city_choices.keys())
+    city_data = data[data.city == city_view]
+    set_view_lat = city_data['latitude'].mean()
+    set_view_long = city_data['longitude'].mean()
+    names = city_data.groupby('name')['name'].count().sort_values(ascending=False)
+    restaurants = st.sidebar.multiselect("Select up to 3 restaurants to display", names.keys())
+    if st.sidebar.button("Generate Map") and len(restaurants)>=1 and len(restaurants)<=3:
+        if len(restaurants) == 1:
+            restaurant1 = city_data[city_data.name == restaurants[0]]
+            map_make_1(set_view_lat, set_view_long, restaurants, restaurant1)
+        elif len(restaurants) == 2:
+            restaurant1 = city_data[city_data.name == restaurants[0]]
+            restaurant2 = city_data[city_data.name == restaurants[1]]
+            map_make_2(set_view_lat, set_view_long, restaurants, restaurant1, restaurant2)
+        else:
+            restaurant1 = city_data[city_data.name == restaurants[0]]
+            restaurant2 = city_data[city_data.name == restaurants[1]]
+            restaurant3 = city_data[city_data.name == restaurants[2]]
+            map_make_3(set_view_lat, set_view_long, restaurants, restaurant1, restaurant2, restaurant3)
+
+def map_make_2(set_view_lat, set_view_long, restaurants, restaurant1, restaurant2):
+    layer1 = pdk.Layer(
+                    'ScatterplotLayer',
+                    data=restaurant1,
+                    get_position='[longitude, latitude]',
+                    get_color='[200, 30, 0, 160]',
+                    get_radius=700
+                    )
+    layer2 = pdk.Layer(
+                    'ScatterplotLayer',
+                    data=restaurant2,
+                    get_position='[longitude,latitude]',
+                    get_color='[0, 200, 30, 160]',
+                    get_radius=700
+                    )
+    st.pydeck_chart(
+        pdk.Deck(
+            map_style='mapbox://styles/mapbox/light-v9',
+            initial_view_state=pdk.ViewState(
+                latitude=set_view_lat,
+                longitude=set_view_long,
+                zoom=9
+            ),
+            layers=[layer1, layer2]
+            )
+        )
+
+
+
+def map_make_3(set_view_lat, set_view_long, restaurants, restaurant1, restaurant2, restaurant3):
+    layer1 = pdk.Layer(
+                    'ScatterplotLayer',
+                    data=restaurant1,
+                    get_position='[longitude, latitude]',
+                    get_color='[200, 30, 0, 160]',
+                    get_radius=700
+                    )
+    layer2 = pdk.Layer(
+                    'ScatterplotLayer',
+                    data=restaurant2,
+                    get_position='[longitude,latitude]',
+                    get_color='[0, 200, 30, 160]',
+                    get_radius=700
+                    )
+    layer3 = pdk.Layer(
+                    'ScatterplotLayer',
+                    data=restaurant3,
+                    get_position='[longitude,latitude]',
+                    get_color='[30, 0, 200, 160]',
+                    get_radius=700
+                )
+    st.pydeck_chart(
+        pdk.Deck(
+            map_style='mapbox://styles/mapbox/light-v9',
+            initial_view_state=pdk.ViewState(
+                latitude=set_view_lat,
+                longitude=set_view_long,
+                zoom=9
+            ),
+            layers=[layer1, layer2, layer3]
+        ))
+
+def map_make_1(set_view_lat, set_view_long, restaurants, restaurant1):
+    st.pydeck_chart(
+        pdk.Deck(
+            map_style='mapbox://styles/mapbox/light-v9',
+            initial_view_state=pdk.ViewState(
+                latitude=set_view_lat,
+                longitude=set_view_long,
+                zoom=9
+            ), layers=[
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=restaurant1,
+                get_position='[longitude, latitude]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=700,
+    )]))
+
 
 def top_five_cities(data):
     top_five_data = (data.groupby('city')['name'].count()).sort_values(ascending=False).head(5)
@@ -127,6 +230,9 @@ def main():
         most_popular_by_state(data)
     elif choice == "Top 5 States With The Most of any Restaurant":
         most_states_with(data)
+    elif choice == "Map of All Restaurants by City":
+        map_choice(data)
 
 
 main()
+
